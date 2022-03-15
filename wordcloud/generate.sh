b3b2871e1acdde4d0011f0f385e312f1c73d2555
Niklas Loeser <51879435+data-niklas@users.noreply.github.com>
2022-03-15 10:38:37 +0100

    improves wordcloud

--- wordcloud/generate.sh
+++ wordcloud/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 find .. -type f | grep -F ".java" | xargs cat | sed '/^$/d' > code.txt
-cat code.txt| wordcloud_cli --mask marklin.jpg --colormask marklin.jpg --imagefile out.jpg
+cat code.txt| wordcloud_cli --mask marklin.jpg --colormask marklin.jpg --stopwords filter.txt --scale 5 --imagefile out.jpg
